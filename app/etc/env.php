28188184d5d1620fabe2f15165eb104fde7cddeb
Mohammad Rohid safi <sroheed@gmail.com>
2023-04-15 11:42:26 +0200

    first commit

---
+++ app/etc/env.php
@@ -0,0 +1,85 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'cache' => [
+        'graphql' => [
+            'id_salt' => 'RxxcZQverXMVYjdPhNQi78H069XM10m3'
+        ],
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69c_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '69c_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'a04b95d39a25ed9c9fec5075190d448f'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'mage',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'lock' => [
+        'provider' => 'db'
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'mage2rock.magento.com'
+    ],
+    'install' => [
+        'date' => 'Fri, 14 Apr 2023 23:49:03 +0000'
+    ]
+];
